80e444adc66c314198bdc4320716293cb32ab7da
CreativeJuiz <CreativeJuiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 17:18:59 +0000

    First Release ! Just try it :)
    git-svn-id: https://plugins.svn.wordpress.org/juiz-user-login-by-email/trunk@990363 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ juiz-user-login-by-email.php
@@ -0,0 +1,81 @@
+<?php
+/*
+	Plugin Name: Juiz User Login by email
+	Plugin URI: http://www.creativejuiz.fr/blog/wordpress/wordpress-autoriser-acces-admin-grace-adresse-e-mail
+	Description: Allows user to log-in with its email address OR its username
+	Version: 1.0.0
+	Author: CreativeJuiz
+	Author URI: http://creativejuiz.com
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/**
+	Copyright 2014 Geoffrey Crofte - Creative Juiz
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+define( 'JULBE_LANG', 'julbe_lang' );
+define( 'JULBE_DIRNAME', basename( dirname( __FILE__ ) ) );
+
+// plugin translatable
+add_action( 'init', 'make_juiz_ulbe_multilang' );
+function make_juiz_ulbe_multilang() {
+	load_plugin_textdomain( JULBE_LANG, false, JULBE_DIRNAME.'/languages' );
+}
+
+// allow auth by email
+if ( !function_exists('juiz_allow_email_login')) {
+	add_filter('authenticate', 'juiz_allow_email_login', 20, 3);
+	/**
+	 * juiz_allow_email_login filter to the authenticate filter hook, to fetch a username based on entered email
+	 * @param  obj $user 			the WP user object
+	 * @param  string $username 	the data of username input
+	 * @param  string $password 	the data of password input
+	 * @return boolean
+	 */
+	function juiz_allow_email_login( $user, $username, $password ) {
+		if ( is_email( $username ) ) {
+			$user = get_user_by_email( $username );
+			if ( $user ) {
+				$username = $user->user_login;
+			}
+		}
+		return wp_authenticate_username_password( null, $username, $password );
+	}
+}
+// allow auth by email text
+if ( !function_exists('juiz_add_email_to_login')) {
+	add_filter( 'gettext', 'juiz_add_email_to_login', 20, 3 );
+	/**
+	 * juiz_add_email_to_login function adds "or email" to the "username" label
+	 * @param string $translated_text   translated text
+	 * @param string $text              original text
+	 * @param string $domain            text domain
+	 */
+	function juiz_add_email_to_login( $translated_text, $text, $domain ) {
+
+		if ( 'wp-login.php' != basename( $_SERVER['SCRIPT_NAME'] ) ) {
+			return $translated_text;
+		}
+
+		if ( "Username" == $text ) {
+			$translated_text .= ' '.__( 'or Email', JULBE_LANG);
+		}
+		return $translated_text;
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,94 @@
+﻿=== User Login by email ===
+Contributors: CreativeJuiz
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=P39NJPCWVXGDY&lc=FR&item_name=Juiz%20Login%20by%20Email%20%2d%20WP%20Plugin&item_number=%23wp%2djulbe&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Tags: login, email, user, wp-login
+Requires at least: 3.8
+Tested up to: 4.0
+Stable tag: 1.0.0
+
+Allow user logs-in with its e-mail OR its username
+
+== Description ==
+
+Add the possibility for user to log-in with its e-mail OR its username.
+Uses native WordPress functions.
+Lightweight plugin, no options, just activate it and it works!
+
+**Please, use the support forum to tell me bugs encountered, before rating the plugin. Be patient, it's free**
+
+
+
+Languages available:
+
+* Deutsch
+* English
+* French
+* Spanish
+* Russian
+
+
+–––––––––––––––––––––––––––––––––––
+
+Français
+
+––
+
+Ajoute la possibilité pour un utilisateur de se connecter avec son adresse e-mail OU son identifiant.
+Utilise les fonctions navites de WordPress.
+Plugin léger, aucune option, il suffit de l'activer et ça marche.
+
+**Merci d'utiliser le forum de support si vous rencontrez des bugs avant de noter le plugin. Soyez patients, c'est gratuit ;)**
+
+
+Langues disponibles :
+
+* Allemand
+* Anglais
+* Espagnol
+* Français
+* Russe
+
+
+
+== Installation ==
+
+You can use one of both method :
+
+**Installation via your Wordpress website**
+
+1. Go to the **admin menu 'Plugins' -> 'Install'** and **search** for 'User login by email'
+1. **Click** 'install' and **activate it**
+1. That's all for you!
+
+**Manual Installation**
+
+1. **Download** the plugin (it's better :p)
+1. **Unzip** `juiz-user-login-by-email` folder to the `/wp-content/plugins/` directory
+1. Go to your WP admin panel to "Appearance > Plugins" and activate
+1. That's all for you!
+
+
+== Frequently Asked Questions ==
+
+**No question for the moment**
+
+
+== Screenshots ==
+
+1. The login box modified
+
+== Other plugins ==
+
+Find my plugins at <a href="http://profiles.wordpress.org/creativejuiz/">http://profiles.wordpress.org/creativejuiz/</a>
+
+
+== Changelog ==
+
+= 1.0.0 =
+* The first release
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Try it ;)
